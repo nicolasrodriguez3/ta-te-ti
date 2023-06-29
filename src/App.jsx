69912ad88a979ad29913fb2a8187b17738eaca7c
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-06-29 18:15:18 -0300

    feat: save to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,20 @@ import { Square } from "./components/Square"
 import { TURNS } from "./constants"
 import { checkWinner } from "./logic/board"
 import WinnerModal from "./components/WinnerModal"
-
+import { deleteGameFromStorage, saveGameInStorage } from "./logic/storage"
 
 function App() {
-	const [board, setBoard] = useState(Array(9).fill(null))
-	const [turn, setTurn] = useState(TURNS.X)
+	const [board, setBoard] = useState(() => {
+		// recuperar partida guardada si existe
+		const boardFromLocalStorage = window.localStorage.getItem("board")
+		if (boardFromLocalStorage) return JSON.parse(boardFromLocalStorage)
+		return Array(9).fill(null)
+	})
+	const [turn, setTurn] = useState(() => {
+		const turnFromLocalStorage = window.localStorage.getItem("turn")
+		return turnFromLocalStorage ?? TURNS.X
+	})
 	const [winner, setWinner] = useState(null)
-
-	
 
 	const updateSquare = (index) => {
 		if (board[index] || winner) return
@@ -22,11 +28,15 @@ function App() {
 		newBoard[index] = turn
 		setBoard(newBoard)
 
+		//actualizar turno
+		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+
 		// revisar si hay ganador
 		const newWinner = checkWinner(newBoard)
 		if (newWinner) {
 			confetti()
 			setWinner(newWinner)
+			deleteGameFromStorage()
 			return
 		}
 		if (newWinner === false) {
@@ -34,18 +44,24 @@ function App() {
 			return
 		}
 
-		setTurn(turn === TURNS.X ? TURNS.O : TURNS.X)
+		//guardar la partida en localStorage
+		saveGameInStorage(newBoard, newTurn)
+
+		setTurn(newTurn)
 	}
 
 	const handleReset = () => {
 		setBoard(Array(9).fill(null))
 		setTurn(TURNS.X)
 		setWinner(null)
+
+		deleteGameFromStorage()
 	}
 
 	return (
 		<>
 			<h1>Ta Te Ti</h1>
+			<button onClick={handleReset}>Reiniciar juego</button>
 			<main className={winner === null ? "board" : "board has-winner"}>
 				{board.map((square, index) => {
 					return (
@@ -59,7 +75,10 @@ function App() {
 				})}
 			</main>
 
-			<WinnerModal winner={winner} handleReset={handleReset} />
+			<WinnerModal
+				winner={winner}
+				handleReset={handleReset}
+			/>
 
 			<section className={winner === null ? "turn" : "turn has-winner"}>
 				<Square className={turn === TURNS.X ? "is-turn" : ""}>{TURNS.X}</Square>
